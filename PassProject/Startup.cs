3b2114c1fbc5eb1e5495ecb5584a517296cc68ae
Keneshov <kgex@mail.ru>
2016-02-15 18:51:56 +0600

    First commit

---
+++ PassProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PassProject.Startup))]
+namespace PassProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
